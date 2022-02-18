ab5e4105209a4b6a246c0876c6c41087e987987d
Akinola Victor <akinolavictor26@gmail.com>
2022-02-18 18:45:39 +0100

    server issues

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const history = require('connect-history-api-fallback');
 const enforce = require('express-sslify');
 
 const app = express();
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
 
 app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(serverStatic({root: __dirname+ '/dist'}));
 app.use(history());
 
-app.listen(port);
+app.listen(process.env.PORT || 5000);
