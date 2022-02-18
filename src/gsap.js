a6f4392fcb4c5ef34a67ec0cc895af98895953ce
AKINOLA Victor <akinolavictor26@gmail.com>
2022-02-18 22:47:31 +0100

    Update gsap.js

--- src/gsap.js
+++ src/gsap.js
@@ -6,7 +6,7 @@ function Animation () {
     const anim2 = gsap.from('.changeTime', {duration: 1.3, delay: 2, opacity: 0})
     const anim3 = gsap.from('.time-and-name', {duration: 1.3, delay: 1, x: '1000%', stagger: .3})
     const anim4 = gsap.from('.control-panel', {duration: 1.3, delay: 1, y: '100%', ease: 'bounce'})
-    const anim5 = gsap.from('.app-container', {duration: .5, x: '101%'})
+    const anim5 = gsap.from('.app-container', {duration: .4, x: '101%'})
 
     let theme = document.querySelector('.theme')
     theme.addEventListener('click', ()=> {
